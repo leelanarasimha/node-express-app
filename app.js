2fb6128bb2c4554e1ce2e44e2bf391f2e87c5a0c
leelanarasimha <leela.narsimha@gmail.com>
2022-09-01 00:46:46 +0530

    Body Parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   console.log('Logging the request');
@@ -11,11 +14,24 @@ app.use((req, res, next) => {
 //   console.log('second middleware');
 // });
 
-app.use('/users', (req, res, next) => {
+app.get('/users', (req, res, next) => {
   res.send('This is the users page');
 });
 
-app.use('/', (req, res, next) => {
+app.get('/user/add', (req, res) => {
+  res.send(`<form method="POST">
+  <div><input name='username'/></div>
+  <div><button>Add User</button></div>
+  </form>`);
+});
+
+app.post('/user/add', (req, res) => {
+  console.log(req.body);
+  console.log('Post request ');
+  res.redirect('/');
+});
+
+app.get('/', (req, res, next) => {
   res.send('<h1>This is the home page</h1>');
 });
 
