120764d4f6998ed04978a5af509c3553e26ed0af
leelanarasimha <leela.narsimha@gmail.com>
2022-09-04 00:48:26 +0530

    Refactor the products data

--- models/Product.js
+++ models/Product.js
@@ -2,25 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../utils/path');
 
+const getProductsFromFile = (callBack) => {
+  const productsPath = path.join(rootDir, 'data', 'products.json');
+  fs.readFile(productsPath, (error, productsData) => {
+    if (error) {
+      return callBack([]);
+    }
+
+    return callBack(JSON.parse(productsData));
+  });
+};
+
 exports.saveProduct = (product) => {
   const productsPath = path.join(rootDir, 'data', 'products.json');
 
-  fs.readFile(productsPath, (error, productsData) => {
-    let products = [];
-    if (!error) {
-      products = JSON.parse(productsData);
-    }
-    products.push(product);
-    fs.writeFile(productsPath, JSON.stringify(products), (error) => {
+  getProductsFromFile((productsData) => {
+    productsData.push(product);
+    fs.writeFile(productsPath, JSON.stringify(productsData), (error) => {
       console.log(error);
     });
   });
 };
 
 exports.fetchAllProducts = (callBack) => {
-  const productsPath = path.join(rootDir, 'data', 'products.json');
-  fs.readFile(productsPath, (error, productsData) => {
-    const products = JSON.parse(productsData);
-    callBack(products);
-  });
+  getProductsFromFile(callBack);
 };
