9767cda2c272f96daae63f7255896dc4115f5105
leelanarasimha <leela.narsimha@gmail.com>
2022-09-03 16:05:10 +0530

    install ejs template engine

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ const homeRoutes = require('./routes/home');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//Static files
 app.use(express.static(path.join(rootDir, 'public')));
-
 app.use('/css', express.static(path.join(rootDir, 'node_modules', 'bootstrap', 'dist', 'css')));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Routes
 app.use(homeRoutes);
 app.use('/users', adminRoutes);
-
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  const viewsData = {
+    pageTitle: 'Page Not Found'
+  };
+  res.status(404).render('404', viewsData);
 });
 
 app.listen(3000, () => {
