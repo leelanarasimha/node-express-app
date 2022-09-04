53e30a11be077d136ad426d59da40a6cb7d70442
leelanarasimha <leela.narsimha@gmail.com>
2022-09-04 16:00:08 +0530

    Create cart page and add to cart button

---
+++ controllers/CartController.js
@@ -0,0 +1,5 @@
+exports.postCartPage = (req, res) => {
+  console.log(req.body);
+};
+
+exports.getCartPage = (req, res) => {};
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { postCartPage, getCartPage } = require('../controllers/CartController');
 const router = express.Router();
 const { getHomePage, getProductDetailsPage } = require('../controllers/HomeController');
 
 router.get('/', getHomePage);
 router.get('/product/details/:productId', getProductDetailsPage);
+
+router.post('/cart', postCartPage);
+router.get('/cart', getCartPage);
 
 module.exports = router;
