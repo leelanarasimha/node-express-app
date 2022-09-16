f0725bd8aada546a0829c8bb434f7ec462543eb6
leelanarasimha <leela.narsimha@gmail.com>
2022-09-16 13:57:03 +0530

    Category save done

--- controllers/admin/CategoryController.js
+++ controllers/admin/CategoryController.js
@@ -1,9 +1,14 @@
-exports.getCategoryPage = (req, res) => {
-  const viewsData = {
-    pageTitle: 'Categories List'
-  };
+const Category = require('../../models/CategoryModel');
 
-  res.render('admin/categories/categoriesPage', viewsData);
+exports.getCategoryPage = (req, res) => {
+  Category.findAll().then((categories) => {
+    const viewsData = {
+      pageTitle: 'Categories List',
+      categories
+    };
+
+    res.render('admin/categories/categoriesPage', viewsData);
+  });
 };
 
 exports.getAddCategoryPage = (req, res) => {
@@ -12,4 +17,17 @@ exports.getAddCategoryPage = (req, res) => {
   };
 
   res.render('admin/categories/addCategoryPage', viewsData);
+};
+
+exports.postAddCategoryPage = (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+
+  Category.create({ title, description })
+    .then(() => {
+      res.redirect('/categories');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { getCategoryPage, getAddCategoryPage } = require('../controllers/admin/CategoryController');
+const {
+  getCategoryPage,
+  getAddCategoryPage,
+  postAddCategoryPage
+} = require('../controllers/admin/CategoryController');
 
 const router = express.Router();
 
 router.get('/', getCategoryPage);
+
 router.get('/add', getAddCategoryPage);
+router.post('/add', postAddCategoryPage);
 
 module.exports = router;
